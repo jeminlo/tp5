25ed13c9f95058b0173e7ae534c47bb667b2c4ce
Administrator <123>
2018-07-25 15:10:31 +0800

    first

---
+++ application/demo/controller/Demo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/7/17
+ * Time: 11:42
+ */
+
+namespace app\demo\controller;
+
+
+class Demo
+{
+    public function index(){
+        return __METHOD__;
+    }
+}
