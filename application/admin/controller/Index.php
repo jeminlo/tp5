43fd36971a440db0165f64c0d1ec1226f72d8a64
Administrator <server>
2018-08-15 16:14:13 +0800

    后台入口文件绑定

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/8/15
+ * Time: 16:11
+ */
+
+namespace app\admin\controller;
+
+
+class Index
+{
+    public function index()
+    {
+        return __METHOD__;
+    }
+
+}
